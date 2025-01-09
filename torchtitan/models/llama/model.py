c9a0f6f5ffe58a1b36b42891f311952321f50fe0
Maximilian Böther <maxi+git@boether.de>
2025-01-09 11:42:19 +0100

    try fixing dtype

--- torchtitan/models/llama/model.py
+++ torchtitan/models/llama/model.py
@@ -487,6 +487,9 @@ class PerDomainLoss(nn.Module):
 
         if key_ids is not None:
             with torch.no_grad():
+                if loss.dtype != self.losses_tensor.dtype:
+                    self.losses_tensor = self.losses_tensor.to(loss.dtype)
+
                 self.has_per_domain_loss = True
 
                 # Flatten key_ids
