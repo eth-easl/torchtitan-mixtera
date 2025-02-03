250a25e7fc66b0c8755a8ad72c617561ceff05bb
Maximilian Böther <maxi+git@boether.de>
2025-02-03 17:24:14 +0100

    tenacity

--- torchtitan/datasets/tokenizer/__init__.py
+++ torchtitan/datasets/tokenizer/__init__.py
@@ -7,10 +7,18 @@
 from torchtitan.datasets.tokenizer.tiktoken import TikTokenizer
 from torchtitan.datasets.tokenizer.tokenizer import Tokenizer
 from torchtitan.datasets.tokenizer.huggingface import HuggingFaceTokenizer
+from tenacity import retry, stop_after_attempt, wait_random_exponential, after_log, before_log
 
 from torchtitan.logging import logger
+import logging
 
-
+@retry( # instantiating huggingface in CSCS is flaky.
+    stop=stop_after_attempt(5),
+    wait=wait_random_exponential(multiplier=1, min=2, max=60),
+    before=before_log(logger, logging.ERROR),
+    after=after_log(logger, logging.ERROR),
+    reraise=True,
+)
 def build_tokenizer(tokenizer_type: str, tokenizer_path: str) -> Tokenizer:
     if tokenizer_type == "tiktoken":
         logger.info(f"Building {tokenizer_type} tokenizer locally from {tokenizer_path}")
