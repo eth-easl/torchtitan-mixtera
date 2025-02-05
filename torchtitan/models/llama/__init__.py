9d5367a379808ba988eccae7ca1e609c7b595f27
Maximilian Böther <maxi+git@boether.de>
2025-02-05 14:51:23 +0100

    add 3b model

--- torchtitan/models/llama/__init__.py
+++ torchtitan/models/llama/__init__.py
@@ -41,7 +41,15 @@ llama3_configs = {
     ),
     "ado": ModelArgs(dim=768, n_layers=12, n_heads=12, max_seq_len=1024),
     "ado1b": ModelArgs(dim=2048, n_layers=24, n_heads=16, max_seq_len=1024, multiple_of=8, ffn_dim_multiplier=None),
-    "doremi500": ModelArgs(dim=1024, n_layers=12, n_heads=16, max_seq_len=1024, ffn_dim_multiplier=8192 / 2730) # enforces hidden dim 8192
-}
+    "doremi500": ModelArgs(dim=1024, n_layers=12, n_heads=16, max_seq_len=1024, ffn_dim_multiplier=8192 / 2730), # enforces hidden dim 8192
+    "3b": ModelArgs(
+        dim=3072,
+        n_layers=28,
+        n_heads=24,
+        n_kv_heads=8,
+        multiple_of=1024,
+        rope_theta=500000, # no ffn_dim_multiplier gives intermediate size 8192
+    ),
+    }
 
 llama3_configs["smollm162m"] = llama3_configs["ado"]
